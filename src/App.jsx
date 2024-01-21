c07fa46b73d9d5107f87fcd13d22358fe16bebe4
NF RIDOY <nfridoy@gmail.com>
2024-01-21 21:55:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-8xl font-bold underline text-primary">
+        
+      </h1>
+    </>
+  )
+}
+
+export default App
