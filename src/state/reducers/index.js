07dea304fe6a439c3bf597946e2dfe8f69de7085
NF RIDOY <nfridoy@gmail.com>
2024-02-14 04:44:07 +0600

    (action creator)

---
+++ src/state/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import amountReducer, { reducer } from "./amountReducers";
+
+export default reducer = combineReducers({
+    amount: amountReducer(amount
+})
